98ade9683636a8af72674402c9430f5a624d39db
Daniel Lim <danheelim@gmail.com>
2019-08-28 17:47:54 -0700

    finished cookie algee :

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,8 +5,17 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n, cache={}):
+  # pass
+  if n == 3:
+        return 4
+  if n == 2:
+        return 2
+  if n <= 1:
+        return 1
+  if n not in cache:
+        cache[n] = eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+  return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
